3201d792a8d502d14e8db4be867d8de52b689dc9
kcsheng <kacysheng@mail.com>
2020-04-26 23:15:36 +1000

    code refactored

--- src/fetch.rb
+++ src/fetch.rb
@@ -7,16 +7,16 @@ class Headlines
         @url = package[0]
         @title = package[1]
         @content = package[2]
+        @unparsed_page = HTTParty.get(@url)
+        @parsed_page = Nokogiri::HTML(@unparsed_page.body)
     end
     def scrape
-        unparsed_page = HTTParty.get(@url)
-        parsed_page = Nokogiri::HTML(unparsed_page.body)
         top_5_news = Hash.new
         (0..4).each do |n|
-            title = parsed_page.css(@title)[n].text.delete("\n""\t")
-            title_content = parsed_page.css(@content)[n].text.delete("\n""\t")
-            top_5_news[title] = title_content
+            headline_title = @parsed_page.css(@title)[n].text.delete("\n""\t")
+            headline_content = @parsed_page.css(@content)[n].text.delete("\n""\t")
+            top_5_news[headline_title] = headline_content
         end
-        return top_5_news
+        top_5_news
     end
 end
