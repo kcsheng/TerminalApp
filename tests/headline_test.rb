2276dfd8daf31a50a41a5d021fa57a33e45a930b
kcsheng <kacysheng@mail.com>
2020-04-26 21:35:57 +1000

    tests completed

--- tests/headline_test.rb
+++ tests/headline_test.rb
@@ -1,11 +1,17 @@
 require "test/unit"
-require_relative "../src/fetch.rb"
+require "./fetch"
 
-
-class HeadlineTest < Test::Unit::TestCase
-    # Check the current top headline on the website and compare it 
-    # against the result from the scrape function
-  
+class LocalHeadlineTest < Test::Unit::TestCase
+    # Check the present top headline on the website and compare it against 
+    # the first key value pair from the scrape function
+    def test_first_headline
+        assert_equal("POWER SHIFT: Meet the woman who could soon be ruler of North Korea", Headlines.new(Nation.new.package).scrape.keys[0])
+    end
+    def test_first_content
+        assert_equal("She’s been described as the “North Korean Ivanka Trump”. Now the young, ambitious Kim Yo Jong is rumoured to take over the hermit nation if her brother dies.", Headlines.new(Nation.new.package).scrape.values[0])
+    end
 end
+
+
 
 
--- tests/user_test.rb
+++ tests/user_test.rb
@@ -1,6 +1,6 @@
 require "test/unit"
-require_relative "../src/user.rb"
-require_relative "../src/package.rb"
+require_relative "./user.rb"
+require_relative "./package.rb"
 
 class LocationTest < Test::Unit::TestCase
     def test_location # Test current user's country and city
@@ -12,7 +12,6 @@ end
 
 class PackageTest < Test::Unit::TestCase
     def test_package # Test current user's national website package 
-        user = User.new
         assert_equal(["https://www.news.com.au/", "h4.heading", "span.standfirst-text"], Nation.new.package)
     end
 end
